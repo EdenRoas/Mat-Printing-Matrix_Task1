65c43f10c3e046f31dc65d89a6f7d9149fae800a
Eden Roas <92533182+EdenRoas@users.noreply.github.com>
2022-04-05 07:04:55 -0400

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -18,12 +18,12 @@ TEST_CASE("Bad input")
 {
     CHECK_THROWS(mat(0, 0, '@', '*')); //1-Empty mat
     CHECK_THROWS(mat(0, 7, '@', '*')); //2-First num = 0
-    CHECK_THROWS(mat(0, 0, '@', '*')); //3-Seconde num = 0
+    CHECK_THROWS(mat(3, 0, '@', '*')); //3-Seconde num = 0
     CHECK_THROWS(mat(2, 7, '@', '*')); //4-First num even
-    CHECK_THROWS(mat(3, 10,'@', '*')); //5-First num are negative & second num are even
-    CHECK_THROWS(mat(7, 9, '@', '*')); //6-Both numbers negative
-    CHECK_THROWS(mat(3, 1, '$', '%')); //7-Second number negative
-    CHECK_THROWS(mat(1, 3, '$', '%')); //8-First number negative
+    CHECK_THROWS(mat(-3, 10,'@', '*')); //5-First num are negative & second num are even
+    CHECK_THROWS(mat(-7, -9, '@', '*')); //6-Both numbers negative
+    CHECK_THROWS(mat(3, -1, '$', '%')); //7-Second number negative
+    CHECK_THROWS(mat(1, 3, ' ', '%')); //8-first char is not valid
     CHECK_THROWS(mat(7, 2, '@', '*')); //9-The second num even
     
     
@@ -91,7 +91,7 @@ TEST_CASE("Good input")
 												    "!---!\n"
 												    "!!!!!"));
 
-     CHECK(nospaces(mat(7, 7, '*', '*')) ==nospaces("*******\n" //18
+    CHECK(nospaces(mat(7, 7, '*', '*')) ==nospaces("*******\n" //18
                                                     "*******\n"
                                                     "*******\n"
                                                     "*******\n"
